2e2127fd46b7b951fbecf5a9d07ec14ecf021f24
Solnarin <atlayanemirhan@gmail.com>
2022-09-18 04:22:33 +0300

    RythymFinish

--- Jam/Assets/Player_G.cs
+++ Jam/Assets/Player_G.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Player_G : MonoBehaviour
 {
     public float Health = 100f;
 
     public Animator anim;
 
-
+    private void Update()
+    {
+        if (Health <= 0)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
     private void BackToIdle()
     {
         anim.SetBool("canAttack", false);
